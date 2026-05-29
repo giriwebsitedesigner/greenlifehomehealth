607d393dd242ee14cee62f719f73df757d085a60
girish-khan910 <girishkhan910@gmail.com>
2026-05-29 13:44:59 +0530

    Update floating contact icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,12 +117,20 @@
   }
 
   function ensureQuickActions() {
-    if (qs(".gl-ios-actions")) return;
-    var wrap = document.createElement("div");
-    wrap.className = "gl-ios-actions";
+    var wrap = qs(".gl-ios-actions");
+    if (!wrap) {
+      wrap = document.createElement("div");
+      wrap.className = "gl-ios-actions";
+      document.body.appendChild(wrap);
+    }
     wrap.setAttribute("aria-label", "Quick contact actions");
-    wrap.innerHTML = '<a class="wa" href="https://wa.me/' + PHONE + '" aria-label="WhatsApp Now"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M17.47 14.38c-.3-.15-1.76-.87-2.03-.97-.27-.1-.47-.15-.67.15-.2.3-.77.97-.94 1.16-.17.2-.35.22-.64.08-.3-.15-1.26-.46-2.39-1.48-.88-.79-1.48-1.76-1.65-2.06-.17-.3-.02-.46.13-.61.13-.13.3-.35.45-.52.15-.17.2-.3.3-.5.1-.2.05-.37-.03-.52-.08-.15-.67-1.61-.92-2.21-.24-.58-.49-.5-.67-.51h-.57c-.2 0-.52.07-.79.37-.27.3-1.04 1.02-1.04 2.48s1.07 2.88 1.21 3.07c.15.2 2.1 3.2 5.08 4.49.71.31 1.26.49 1.69.63.71.23 1.36.2 1.87.12.57-.09 1.76-.72 2.01-1.41.25-.69.25-1.29.17-1.41-.07-.12-.27-.2-.57-.35z"/><path d="M12 2.25A9.75 9.75 0 0 0 3.77 17.2l.24.38-.9 2.62 2.69-.89.35.21A9.75 9.75 0 1 0 12 2.25m0-2.25a12 12 0 0 1 0 24 11.96 11.96 0 0 1-5.84-1.51l-4.46 1.5a.75.75 0 0 1-.92-.92l1.5-4.46A12 12 0 0 1 12 0z"/></svg></a><a class="phone" href="tel:+916383915367" aria-label="Call Now"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M6.62 10.79a15.05 15.05 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.01-.24c1.12.37 2.33.57 3.58.57a1 1 0 0 1 1 1V21a1 1 0 0 1-1 1C10.85 22 2 13.15 2 2a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.46.57 3.58a1 1 0 0 1-.25 1.01l-2.2 2.2z"/></svg></a>';
-    document.body.appendChild(wrap);
+    wrap.innerHTML =
+      '<a class="wa" href="https://wa.me/' + PHONE + '" aria-label="WhatsApp Now">' +
+        '<img src="' + assetUrl("icons/float-whatsapp.svg?v=3") + '" alt="">' +
+      '</a>' +
+      '<a class="phone" href="tel:+916383915367" aria-label="Call Now">' +
+        '<img src="' + assetUrl("icons/float-phone.svg?v=3") + '" alt="">' +
+      '</a>';
   }
 
   ensureQuickActions();
